dabf657541511ebe12c1393f3f70200a5c63510d
Ivan <antipchel@gmail.com>
2021-08-02 16:03:07 +0300

    added information

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,9 @@ import Table from './Table.jsx';
 export default () => (
   <Provider store={store()}>
     <div className="container">
+      <p className="my-3 text-center fs-5 alert alert-info">
+        Введите идентификаторы строк через запятую или точку с запятой и нажмите кнопку
+      </p>
       <div className="my-3">
         <Input />
       </div>
