fffbf13a1eea382cb6b0f1e85f235968f0228be5
Ivan <antipchel@gmail.com>
2021-08-09 21:03:11 +0300

    fixed linter errors

--- src/components/InputLogic.js
+++ src/components/InputLogic.js
@@ -1,20 +1,22 @@
-export const parseCsv = (str, includeDelimitersAndEmpty = true) => (includeDelimitersAndEmpty === true
-  ? str.match(/[,;]+|[^,^;]+/g)
-  : str.split(/[,;]+/).filter((str) => str.length !== 0)) ?? [];
+export const parseCsv = (str, includeDelimitersAndEmpty = true) => (
+  (includeDelimitersAndEmpty === true
+    ? str.match(/[,;]+|[^,^;]+/g)
+    : str.split(/[,;]+/).filter((str2) => str2.length !== 0)) ?? []);
 // detect numbers
 export const isNumberFits = (numberStr, min = 1, max = 20) => {
   if (/^\s*[0-9]+\s*$/.test(numberStr) === false) return false;
-  const number = Number.parseInt(numberStr);
+  const number = Number.parseInt(numberStr, 10);
   return number >= min && number <= max;
 };
 
-export const filterParsedCsv = (strArray, removeDuplicates = true) => strArray.reduce((acc, str) => {
-  if (isNumberFits(str) === true
-    && (removeDuplicates === false || acc.includes(parseInt(str)) === false)) { 
-      acc.push(parseInt(str)); 
+export const filterParsedCsv = (strArray, removeDuplicates = true) => (
+  strArray.reduce((acc, str) => {
+    if (isNumberFits(str) === true
+      && (removeDuplicates === false || acc.includes(parseInt(str, 10)) === false)) {
+      acc.push(parseInt(str, 10));
     }
-  return acc;
-}, []);
+    return acc;
+  }, []));
 
 // this code is stackoverflow one
 export const saveCaretPosition = (el) => {
@@ -24,7 +26,8 @@ export const saveCaretPosition = (el) => {
   const len = range.toString().length;
 
   return () => {
-    const getTextNodeAtPosition = (rootEl, index) => {
+    const getTextNodeAtPosition = (rootEl, constIndex) => {
+      let index = constIndex;
       const NODE_TYPE = NodeFilter.SHOW_TEXT;
       const treeWalker = document.createTreeWalker(rootEl, NODE_TYPE, (elem) => {
         if (index > elem.textContent.length) {
@@ -41,8 +44,8 @@ export const saveCaretPosition = (el) => {
     };
     const pos = getTextNodeAtPosition(el, len);
     selection.removeAllRanges();
-    const range = new Range();
-    range.setStart(pos.node, pos.position);
-    selection.addRange(range);
+    const savedRange = new Range();
+    savedRange.setStart(pos.node, pos.position);
+    selection.addRange(savedRange);
   };
 };
--- src/components/TableLogic.js
+++ src/components/TableLogic.js
@@ -1,31 +1,36 @@
-import regeneratorRuntime from "regenerator-runtime";
 import { startLoading, errorLoading, successLoading } from '../slices/app.js';
 import offlineSamples from '../offlineSamples.js';
 
 // async
-const combineAsync = async (fetches) => 
-  await Promise.all(fetches);
+const combineAsync = async (fetches) => Promise.all(fetches);
 
-const getJsonAsync = async (responses) => 
-  await combineAsync(responses.map((response) => response.json()));
+const getJsonAsync = async (responses) => (
+  combineAsync(responses.map((response) => response.json())));
 
 // all-in-one function
 export const processCsvAsync = async (csv, dispatch, useServer = false) => {
   if (useServer === true || window.useServer === true) {
     dispatch(startLoading());
-    
-    let protocol = (location.protocol === 'https:') ? 'https' : 'http';
-    let link = `${protocol}://tmgwebtest.azurewebsites.net/api/textstrings/`;
 
-    const responses = await combineAsync(csv.map((number) => fetch(`${link}${number}`,
-      { headers: { 'TMG-Api-Key': '0J/RgNC40LLQtdGC0LjQutC4IQ==' } })));
+    const protocol = (location.protocol === 'https:') ? 'https' : 'http';
+    const link = `${protocol}://tmgwebtest.azurewebsites.net/api/textstrings/`;
+    try {
+      const responses = await combineAsync(csv.map((number) => fetch(`${link}${number}`,
+        { headers: { 'TMG-Api-Key': '0J/RgNC40LLQtdGC0LjQutC4IQ==' } })));
 
-    const filteredResponses = filterErrors(dispatch)(responses);
-    const JSONs = await getJsonAsync(filteredResponses);
-    // console.log(JSONs);
-    const textsWithStats = JSONs.map((JSON) => textStats(JSON.text));
-    // console.log(textsWithStats);
-    dispatch(successLoading(textsWithStats));
+      const filteredResponses = filterErrors(dispatch, responses);
+      const JSONs = await getJsonAsync(filteredResponses);
+      // console.log(JSONs);
+      const textsWithStats = JSONs.map((JSON) => textStats(JSON.text));
+      // console.log(textsWithStats);
+      dispatch(successLoading(textsWithStats));
+    } catch (error) {
+      if (error.message.toLowerCase().includes('network')) {
+        dispatch(errorLoading('Ошибка сети при попытке связи с сервером'));
+      } else {
+        dispatch(errorLoading(`Ошибка ${error.message}`));
+      }
+    }
   } else {
     dispatch(startLoading());
     const JSONs = offlineSamples.filter((_, i) => csv.includes(i));
@@ -34,9 +39,9 @@ export const processCsvAsync = async (csv, dispatch, useServer = false) => {
     // console.log(textsWithStats);
     dispatch(successLoading(textsWithStats));
   }
-}
+};
 // sync
-const filterErrors = (dispatch) => (responses) => responses.reduce((acc, response) => {
+const filterErrors = (dispatch, responses) => responses.reduce((acc, response) => {
   switch (response.status) {
     case 200:
       acc.push(response);
@@ -53,15 +58,9 @@ const filterErrors = (dispatch) => (responses) => responses.reduce((acc, respons
   }
   return acc;
 }, []);
+
 const textStats = (text) => ({
   text,
   wordCount: (text.match(/[a-zа-я']+-*[a-zа-я']*/gi) ?? []).length,
   vowelCount: (text.match(/[AEIOUаиеёоуыэюяáéýíóúæøåÆÅäöü]/gi) ?? []).length,
 });
-const fetchCatcher = (dispatch) => (error) => {
-  if (error.message.toLowerCase().includes('network')) { 
-    dispatch(errorLoading('Ошибка сети при попытке связи с сервером')); 
-  } else { 
-    dispatch(errorLoading(`Ошибка ${error.message}`)); 
-  }
-};
